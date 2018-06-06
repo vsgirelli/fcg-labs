d00e2f8cf06f85d5facf2652d6ed7e76b8526bf4
vsgirelli <vavi_sg@hotmail.com>
2018-06-05 21:33:50 -0300

    Adding version with name

--- 5lab/Laboratorio_5_Codigo_Fonte/src/main.cpp
+++ 5lab/Laboratorio_5_Codigo_Fonte/src/main.cpp
@@ -219,7 +219,7 @@ int main(int argc, char* argv[])
     // Criamos uma janela do sistema operacional, com 800 colunas e 600 linhas
     // de pixels, e com título "INF01047 ...".
     GLFWwindow* window;
-    window = glfwCreateWindow(800, 600, "INF01047 - Seu Cartao - Seu Nome", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "INF01047 - 00261596 - Valéria S. Girelli", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
