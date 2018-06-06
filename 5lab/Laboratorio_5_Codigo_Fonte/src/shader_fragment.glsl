e8424e304b0e70ba5e87761daaea0b40c08fa2b5
vsgirelli <vavi_sg@hotmail.com>
2018-06-05 22:05:17 -0300

    Adding lights texture

--- 5lab/Laboratorio_5_Codigo_Fonte/src/shader_fragment.glsl
+++ 5lab/Laboratorio_5_Codigo_Fonte/src/shader_fragment.glsl
@@ -126,11 +126,12 @@ void main()
 
     // Obtemos a refletância difusa a partir da leitura da imagem TextureImage0
     vec3 Kd0 = texture(TextureImage0, vec2(U,V)).rgb;
+    vec3 Kd1 = texture(TextureImage1, vec2(U,V)).rgb;
 
     // Equação de Iluminação
     float lambert = max(0,dot(n,l));
 
-    color = Kd0 * (lambert + 0.01);
+    color = Kd1 * (1 - pow(lambert,0.2)) +  Kd0 * (lambert + 0.01);
 
     // Cor final com correção gamma, considerando monitor sRGB.
     // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
